53aa30ab6031da9d68776e842155ef9051130ca6
김형원 <twins@gimhyeong-won-ui-MacBookPro.local>
2025-09-05 14:31:36 +0900

    fix: Remove Babel transform-runtime plugin to resolve Metro bundler issues
    - Simplify babel.config.js to only use babel-preset-expo
    - Remove transform-runtime plugin that was causing @babel/runtime module resolution issues
    - This should allow Metro to bundle properly for web platform without runtime import errors

--- apps/student/babel.config.js
+++ apps/student/babel.config.js
@@ -2,15 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['@babel/plugin-transform-runtime', {
-        helpers: false,
-        regenerator: true,
-        corejs: false,
-        useESModules: false,
-        absoluteRuntime: false,
-        version: '^7.25.2'
-      }]
-    ],
   };
 };
